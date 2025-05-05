4a5f909136d0976fd0cacfd8badfc5746284dff4
Zyorel <dftorresr.1@gmail.com>
2025-05-04 20:26:29 -0500

    Added áge colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,19 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./src/**/*.{js,jsx,ts,tsx}",
-  ],
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: {
+          100: "#EEE3FF",
+          600: "#8054C7",
+          700: "#5A3696",
+        },
+        secondary: {
+          600: "#63D838",
+        },
+      },
+    },
   },
   plugins: [],
-}
-
+};
